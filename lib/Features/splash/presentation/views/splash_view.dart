8942d96c93b9faeb830ae68bbc9161940efef8f6
samahussien <samamego15@gmail.com>
2024-07-26 18:55:00 +0300

    new app

---
+++ lib/Features/splash/presentation/views/splash_view.dart
@@ -0,0 +1,14 @@
+import 'package:bookly/Features/splash/presentation/views/widgets/splash_view_body.dart';
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    //test comment
+    return const  Scaffold(
+      body:  SplashPageBody(),
+    );
+  }
+}
