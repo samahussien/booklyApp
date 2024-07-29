0cdb2ef8ad4e10ae0ed5ea93cc3e108e09ca38ec
samahussien <samamego15@gmail.com>
2024-07-29 22:34:58 +0300

    best seller list item

--- lib/Features/home/presentation/views/widgets/homepage_body.dart
+++ lib/Features/home/presentation/views/widgets/homepage_body.dart
@@ -1,6 +1,7 @@
 import 'package:bookly/Features/home/presentation/views/widgets/custom_appbar.dart';
 import 'package:bookly/Features/home/presentation/views/widgets/custom_listview.dart';
 import 'package:bookly/constants.dart';
+import 'package:bookly/core/utils/assets.dart';
 import 'package:bookly/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -11,10 +12,45 @@ class HomePageBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Padding(
-      padding: EdgeInsets.only(left:kHorizontalPadding ),
+      padding: EdgeInsets.only(left: kHorizontalPadding),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-        children: [CustomAppBar(), CustomListView(),SizedBox(height: 30,),Text("Best Seller",style: Styles.titileMeduim,)],
+        children: [
+          CustomAppBar(),
+          CustomListView(),
+          SizedBox(
+            height: 30,
+          ),
+          Text(
+            "Best Seller",
+            style: Styles.titleGt,
+          ),
+          BestSellerListViewItem()
+        ],
+      ),
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 125,
+      child: Row(
+        children: [
+          AspectRatio(
+              aspectRatio: 2.7 / 4,
+              child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                      color: Colors.white,
+                      image: DecorationImage(
+                        fit: BoxFit.fill,
+                          image: AssetImage(AssetsData.testImage)))))
+        ],
       ),
     );
   }
