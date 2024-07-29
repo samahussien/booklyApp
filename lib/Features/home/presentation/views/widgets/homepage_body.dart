3600a96abb88c9621e3bd1a4b367915e7af2fd56
samahussien <samamego15@gmail.com>
2024-07-29 23:36:06 +0300

    book rating

--- lib/Features/home/presentation/views/widgets/homepage_body.dart
+++ lib/Features/home/presentation/views/widgets/homepage_body.dart
@@ -1,10 +1,9 @@
+import 'package:bookly/Features/home/presentation/views/widgets/best_seller_listview_item.dart.dart';
 import 'package:bookly/Features/home/presentation/views/widgets/custom_appbar.dart';
 import 'package:bookly/Features/home/presentation/views/widgets/custom_listview.dart';
 import 'package:bookly/constants.dart';
-import 'package:bookly/core/utils/assets.dart';
 import 'package:bookly/core/utils/styles.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class HomePageBody extends StatelessWidget {
   const HomePageBody({super.key});
@@ -12,7 +11,7 @@ class HomePageBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Padding(
-      padding: EdgeInsets.only(left: kHorizontalPadding),
+      padding: EdgeInsets.symmetric(horizontal: kHorizontalPadding,),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -23,7 +22,7 @@ class HomePageBody extends StatelessWidget {
           ),
           Text(
             "Best Seller",
-            style: Styles.titleGt,
+            style: Styles.textStyle18,
           ),
           BestSellerListViewItem()
         ],
@@ -32,26 +31,3 @@ class HomePageBody extends StatelessWidget {
   }
 }
 
-class BestSellerListViewItem extends StatelessWidget {
-  const BestSellerListViewItem({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 125,
-      child: Row(
-        children: [
-          AspectRatio(
-              aspectRatio: 2.7 / 4,
-              child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8),
-                      color: Colors.white,
-                      image: DecorationImage(
-                        fit: BoxFit.fill,
-                          image: AssetImage(AssetsData.testImage)))))
-        ],
-      ),
-    );
-  }
-}
--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -10,7 +10,7 @@ abstract class Styles {
   static const textStyle18 =
       TextStyle(fontSize: 18, fontWeight: FontWeight.w600);
   static const textStyle20 = TextStyle(
-      fontSize: 20, fontWeight: FontWeight.normal, fontFamily: kGTSectra);
+      fontSize: 20, fontWeight: FontWeight.normal);
   static const textStyle30 = TextStyle(
       fontSize: 30, fontWeight: FontWeight.normal, fontFamily: kGTSectra);
 }
