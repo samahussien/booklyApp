13a48ecbd92c99e4e351da873a587831e73f52b2
samahussien <samamego15@gmail.com>
2024-10-01 23:52:52 +0300

    test stash

---
+++ lib/Features/search/presentation/manager/search_cubit/search_cubit.dart
@@ -0,0 +1,7 @@
+import 'package:bookly/Features/search/presentation/manager/search_cubit/search_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+class SearchCubit extends Cubit<SearchState> {
+  SearchCubit() : super(SearchInitialState());
+  
+
+}
---
+++ lib/Features/search/presentation/manager/search_cubit/search_state.dart
@@ -0,0 +1,8 @@
+abstract class SearchState {
+
+}
+
+class SearchInitialState extends SearchState {}
+class SearchLoadingState extends SearchState {}
+class SearchSuccessState extends SearchState {}
+class SearchFailureState extends SearchState {}
