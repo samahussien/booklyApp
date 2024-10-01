ebbbe8f40f3f3883ee864a419fc2671373c96a91
samahussien <samamego15@gmail.com>
2024-10-01 23:14:52 +0300

    lunch url

--- lib/Features/home/presentation/views/widgets/book_details_widgets/book_actions.dart
+++ lib/Features/home/presentation/views/widgets/book_details_widgets/book_actions.dart
@@ -1,18 +1,21 @@
+import 'package:bookly/Features/home/data/models/book_model/book_model.dart';
 import 'package:bookly/core/widgets/custom_button.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class BooksAction extends StatelessWidget {
   const BooksAction({
     super.key,
+    required this.bookModel,
   });
-
+  final BookModel bookModel;
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 8.0),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
       child: Row(
         children: [
-          Expanded(
+          const Expanded(
               child: CustomButton(
             text: "Free",
             backgroundColor: Colors.white,
@@ -24,9 +27,17 @@ class BooksAction extends StatelessWidget {
           Expanded(
               child: CustomButton(
             text: "Preview",
-            backgroundColor: Color(0xffEF8262),
+            onPressed: () async {
+              Uri uri = Uri.parse(bookModel.volumeInfo.previewLink!);
+              if (await canLaunchUrl(uri)) {
+                await launchUrl(uri);
+              } else {
+                throw Exception("couldn't launch");
+              }
+            },
+            backgroundColor: const Color(0xffEF8262),
             textColor: Colors.white,
-            borderRadius: BorderRadius.only(
+            borderRadius: const BorderRadius.only(
                 topRight: Radius.circular(16),
                 bottomRight: Radius.circular(16)),
             fontSize: 16,
--- lib/Features/home/presentation/views/widgets/book_details_widgets/book_details_sections/book_details_section.dart
+++ lib/Features/home/presentation/views/widgets/book_details_widgets/book_details_sections/book_details_section.dart
@@ -49,7 +49,7 @@ class BookDetailsSection extends StatelessWidget {
         const SizedBox(
           height: 37,
         ),
-        const BooksAction(),
+         BooksAction(bookModel: book,),
       ],
     );
   }
